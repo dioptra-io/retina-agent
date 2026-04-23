b618aa8e4a4acc7904860184889e9974999b679e
Elena Nardi <elena.nardi@lip6.fr>
2026-04-23 17:16:14 +0200

    chore: replace Dioptra with Sorbonne Université in copyright headers

--- cmd/mock-orchestrator/main_test.go
+++ cmd/mock-orchestrator/main_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Dioptra
+// Copyright (c) 2025 Sorbonne Université
 // SPDX-License-Identifier: MIT
 
 // All functions reach 100% coverage except:
--- cmd/retina-agent/main.go
+++ cmd/retina-agent/main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Dioptra
+// Copyright (c) 2025 Sorbonne Université
 // SPDX-License-Identifier: MIT
 
 // Command retina-agent is a network measurement agent that connects to
